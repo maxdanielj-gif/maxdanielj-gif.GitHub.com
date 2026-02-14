93e44e5764bfae054a13422499e34d3207692f41
maxdanielj-gif <maxdanielj@gmail.com>
2026-02-14 00:48:33 -0500

    Add files via upload

---
+++ utils/fileUtils.ts
@@ -0,0 +1,18 @@
+
+export const fileToBase64 = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = error => reject(error);
+    });
+};
+
+export const textFileToString = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = error => reject(error);
+    });
+};
